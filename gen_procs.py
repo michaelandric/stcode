0b7727a15dbaca28a899eec2f6bb0be42dc55f74
Michael Andric <andric.michael@gmail.com>
2015-02-05 17:36:27 +0100

    testing version with just ANGO

--- gen_procs.py
+++ gen_procs.py
@@ -4,7 +4,8 @@ import os
 import shutil
 from glob import glob
 
-subjects = ["ANGO", "MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN","CLFR"]
+#subjects = ["ANGO", "MYTP","TRCO","PIGL","SNNW","LDMW","FLTM","EEPA","DNLN","CRFO","ANMS","MRZM","MRVV","MRMK","MRMC","MRAG","MNGO","LRVN","CLFR"]
+subjects = ["ANGO"]   # trying on one person
 
 def make_dirs(newdir):
     if os.path.exists(newdir):
@@ -17,6 +18,9 @@ def file_mover(file, destination):
     shutil.copy2(file, destination)
 
 def generate_batchargs(arg1, arg2, arg3):
+    """sample usage:
+    $ python gen_procs.py >> ../submitdir/submit_1.corr_mats.py
+    """
     print 'arguments    = %s %s %s \nqueue \n' % (arg1, arg2, arg3)
 
 
