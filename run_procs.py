84f8cf0b75b3d7624d7ed7215c6dc1fde38a7b09
Michael Andric <andric.michael@gmail.com>
2015-03-23 16:23:22 +0100

    running procs in loop not condor

--- run_procs.py
+++ run_procs.py
@@ -12,23 +12,24 @@ import procedures as procs
 
 if __name__ == '__main__':
 
-    # subj_list = ['ANGO', 'CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MNGO', 'LRVN']
+    subj_list = ['CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN']
+    # subj_list = ['CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MNGO', 'LRVN']
     # subj_list = ['ANGO']
     """for ss in subj_list:
         arglist = []
         arglist.append('run_procs.py')
         arglist.append(ss)
         procs.gen_condor_submit_args(arglist)"""
+    # ss = sys.argv[1]
+    for ss in subj_list:
+        # For undump:
+        ijks = '%s/%s/masking/ijk_coords_graymattermask_%s' % (os.environ['state_rec'], ss, ss)
+        snsc_data = 'snsc_%s.txt' % ss
+        data_dir = '%s/state/snsc_results' % os.environ['t2']
+        mstr = '%s/%s/blur.1.%s.steadystate.TRIM+orig' % (os.environ['state_rec'], ss, ss)
+        procs.undump(ss, ijks, snsc_data, data_dir, mstr)
 
-    ss = sys.argv[1]
-    # For undump:
-    ijks = '%s/%s/masking/ijk_coords_graymattermask_%s' % (os.environ['state_rec'], ss, ss)
-    snsc_data = 'snsc_%s.txt' % ss
-    data_dir = '%s/state/snsc_results' % os.environ['t2']
-    mstr = '%s/%s/blur.1.%s.steadystate.TRIM+orig' % (os.environ['state_rec'], ss, ss)
-    procs.undump(ss, ijks, snsc_data, data_dir, mstr)
-
-    # for autotlrc
-    tlrc_brain = '%s/%s/corrTRIM_BLUR/%stlrc+tlrc' % (os.environ['state_rec'], ss, ss)
-    afni_dat = '%s.ijk+orig' % snsc_data
-    procs.autotlrc(ss, tlrc_brain, afni_dat, data_dir)
+        # for autotlrc
+        tlrc_brain = '%s/%s/corrTRIM_BLUR/%stlrc+tlrc' % (os.environ['state_rec'], ss, ss)
+        afni_dat = '%s.ijk+orig' % snsc_data
+        procs.autotlrc(ss, tlrc_brain, afni_dat, data_dir)
