c73460c1b11e598e7190819efe53213269ee5d0c
Michael Andric <andric.michael@gmail.com>
2015-03-23 22:42:09 +0100

    undump snsc_group_median result

--- run_procs.py
+++ run_procs.py
@@ -12,22 +12,14 @@ import procedures as procs
 
 if __name__ == '__main__':
 
-    subj_list = ['CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
+    # subj_list = ['CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
     """for ss in subj_list:
         arglist = []
         arglist.append('run_procs.py')
         arglist.append(ss)
         procs.gen_condor_submit_args(arglist)"""
-    # ss = sys.argv[1]
-    for ss in subj_list:
-        # For undump:
-        ijks = '%s/%s/masking/ijk_coords_graymattermask_%s' % (os.environ['state_rec'], ss, ss)
-        snsc_data = 'snsc_%s.txt' % ss
-        data_dir = '%s/state/snsc_results' % os.environ['t2']
-        mstr = '%s/%s/blur.1.%s.steadystate.TRIM+orig' % (os.environ['state_rec'], ss, ss)
-        procs.undump(ss, ijks, snsc_data, data_dir, mstr)
-
-        # for autotlrc
-        tlrc_brain = '%s/%s/corrTRIM_BLUR/%stlrc+tlrc' % (os.environ['state_rec'], ss, ss)
-        afni_dat = '%s.ijk+orig' % snsc_data
-        procs.autotlrc(ss, tlrc_brain, afni_dat, data_dir)
+    data_dir = '%s/state/snsc_results' % os.environ['t2']
+    ijks = '%s/groupstats/ijk_coords_TTavg152T1' % os.environ['state_rec']
+    dat = '%s/snsc_group_median.txt' % data_dir
+    mstr = '%s/groupstats/automask_d1_TTavg152T1+tlrc' % os.environ['state_rec']
+    procs.undump('preserved', ijks, dat, data_dir, mstr)
