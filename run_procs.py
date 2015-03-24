9c08e792d82e0a20e4b61f4d417c8b80cf129fa5
Michael Andric <andric.michael@gmail.com>
2015-03-24 17:17:01 +0100

    undump avg_corrZ files

--- run_procs.py
+++ run_procs.py
@@ -12,19 +12,16 @@ import procedures as procs
 
 if __name__ == '__main__':
 
-    # subj_list = ['CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
+    # subj_list = ['ANGO', 'CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
     """for ss in subj_list:
         arglist = []
         arglist.append('run_procs.py')
         arglist.append(ss)
         procs.gen_condor_submit_args(arglist)"""
-    """data_dir = '%s/state/snsc_results' % os.environ['t2']
-    ijks = '%s/groupstats/ijk_coords_TTavg152T1' % os.environ['state_rec']
-    dat = '%s/snsc_group_median.txt' % data_dir
-    mstr = '%s/groupstats/automask_d1_TTavg152T1+tlrc' % os.environ['state_rec']
-    procs.undump('preserved', ijks, dat, data_dir, mstr)"""
-    for h in ['lh', 'rh']:
-        parent_vol = 'snsc_group_median.txt.ijk+tlrc'
-        pn = '2.0'
-        outname = 'snsc_group_median.txt.ijk_tlrc_%s_%s.1D' % (h, pn)
-        procs.vol2surf(h, parent_vol, 'TT_N27+tlrc', '2.0', outname)
+    ss = sys.argv[1]
+    data_dir = '%s/state/global_connectivity' % os.environ['t2']
+    for cc in xrange(1, 5):
+        ijks = '%s/%s/masking/ijk_coords_graymattermask_%s' % (os.environ['state_rec'], ss, ss)
+        dat = '%s/avg_corrZ_%d_%s' % (data_dir, cc, ss)
+        mstr = '%s/%s/blur.%d.%s.steadystate.TRIM+orig' % (os.environ['state_rec'], ss, cc, ss)
+        procs.undump(ss, ijks, dat, data_dir, mstr)
