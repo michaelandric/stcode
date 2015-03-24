1721ea4bec90c077e3b05e6148ddd631edae1eb9
Michael Andric <andric.michael@gmail.com>
2015-03-24 13:52:01 +0100

    undump snsc_group_median with 777 filter

--- run_procs.py
+++ run_procs.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
         arglist.append('run_procs.py')
         arglist.append(ss)
         procs.gen_condor_submit_args(arglist)"""
-    """data_dir = '%s/state/snsc_results' % os.environ['t2']
+    data_dir = '%s/state/snsc_results' % os.environ['t2']
     ijks = '%s/groupstats/ijk_coords_TTavg152T1' % os.environ['state_rec']
     dat = '%s/snsc_group_median.txt' % data_dir
     mstr = '%s/groupstats/automask_d1_TTavg152T1+tlrc' % os.environ['state_rec']
-    procs.undump('preserved', ijks, dat, data_dir, mstr)"""
-    for h in ['lh', 'rh']:
+    procs.undump('preserved', ijks, dat, data_dir, mstr)
+    """for h in ['lh', 'rh']:
         parent_vol = 'snsc_group_median.txt.ijk+tlrc'
         pn = '2.0'
         outname = 'snsc_group_median.txt.ijk_tlrc_%s_%s.1D' % (h, pn)
-        procs.vol2surf(h, parent_vol, 'TT_N27+tlrc', '2.0', outname)
+        procs.vol2surf(h, parent_vol, 'TT_N27+tlrc', '2.0', outname)"""
