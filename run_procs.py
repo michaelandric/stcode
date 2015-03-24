81166609ed755013810f0ff47443d6b86cc47f35
Michael Andric <andric.michael@gmail.com>
2015-03-25 00:03:12 +0100

    trying fsl_anat serially
    There are no errors being given back trying to do this via condor
    cluster but they’re terminating

--- run_procs.py
+++ run_procs.py
@@ -14,9 +14,11 @@ from glob import glob
 
 if __name__ == '__main__':
 
-    # subj_list = ['ANGO', 'CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
-    ss = sys.argv[1]
-    anat_dir = '%s/state/%s' % (os.environ['t2'], ss)
-    os.chdir(anat_dir)
-    print os.getcwd()
-    procs.fslanat('%s.SurfVol_Alnd_Exp' % ss)
+    subj_list = ['ANGO', 'CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW']
+    # subj_list = ['LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
+    # ss = sys.argv[1]
+    for ss in subj_list:
+        anat_dir = '%s/state/%s' % (os.environ['t2'], ss)
+        os.chdir(anat_dir)
+        print os.getcwd()
+        procs.fslanat('%s.SurfVol_Alnd_Exp' % ss)
