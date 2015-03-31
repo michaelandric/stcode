1190a56bb0e5cfd1e98203dbb0b5d37f84d38d47
Michael Andric <andric.michael@gmail.com>
2015-03-31 12:39:50 +0200

    mask dump for neglin_tstat mask

--- run_procs.py
+++ run_procs.py
@@ -15,10 +15,9 @@ import sys
 if __name__ == '__main__':
 
     # subj_list = ['ANGO', 'CLFR', 'MYTP', 'TRCO', 'PIGL', 'SNNW', 'LDMW', 'FLTM', 'EEPA', 'DNLN', 'CRFO', 'ANMS', 'MRZM', 'MRVV', 'MRMK', 'MRMC', 'MRAG', 'MNGO', 'LRVN']
-    ss = sys.argv[1]
+    # ss = sys.argv[1]
     mask = '%s/data/standard/MNI152_T1_2mm_brain_mask_dil1.nii.gz' % os.environ['FSLDIR']
-    data_dir = '%s/state/global_connectivity/%s_res' % (os.environ['t2'], ss)
-    for cc in xrange(1, 5):
-        afni_data_pref = 'avg_corrZ_%d_%s_highres_fnirted_MNI2mm.nii.gz' % (cc, ss)
-        outname_pref = 'avg_corrZ_%d_%s_highres_fnirted_MNI2mm.txt' % (cc, ss)
-        procs.maskdump(mask, data_dir, os.path.join(data_dir, afni_data_pref), os.path.join(data_dir, outname_pref))
+    data_dir = '%s/state/global_connectivity/group' % os.environ['t2']
+    afni_data_pref = 'neglin_tstat_avg_corrZ_anova_Clust_mask+tlrc'
+    outname_pref = 'neglin_tstat_avg_corrZ_anova_Clust_mask+tlrc.txt'
+    procs.maskdump(mask, data_dir, os.path.join(data_dir, afni_data_pref), os.path.join(data_dir, outname_pref))
